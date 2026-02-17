1172327afbc88976a77a66cd7d276188071478bc
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-02-17 21:30:47 +0530

    After Exams only again Final

--- auth.js
+++ auth.js
@@ -4,26 +4,33 @@ const SUPABASE_KEY = "sb_publishable_ku2-MLJitQBIjO3Oge6JZA_FlXTl87I";
 const supbase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const loginForm = document.getElementById("loginForm");
-
+const logbut = document.getElementById("logbut");
 loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
+  logbut.disabled = true;
+  logbut.textContent="Geting your things Ready....";
+  try{
+        const email = document.getElementById("loginEmail").value.trim();
+      const password = document.getElementById("loginPass").value;
 
-  const email = document.getElementById("loginEmail").value.trim();
-  const password = document.getElementById("loginPass").value;
+      const { data, error } = await supbase.auth.signInWithPassword({
+        email,
+        password,
+      });
 
-  const { data, error } = await supbase.auth.signInWithPassword({
-    email,
-    password,
-  });
+      if (error) {
+        alert(error.message);
+        return;
+      }
 
-  if (error) {
-    alert(error.message);
-    return;
+      console.log("Logged in:", data.user?.email);
+
+      window.location.href = "app.html";
+
+      console.log("Logged in:", data.user?.email);
+  }finally {
+    logbut.disabled= false;
+    logbut.textContent = "Login";
   }
-
-  console.log("Logged in:", data.user?.email);
-
-  window.location.href = "app.html";
-
-  console.log("Logged in:", data.user?.email);
+  
 });
--- signup.js
+++ signup.js
@@ -5,6 +5,8 @@ const supbase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const signupForm = document.getElementById("signupForm");
 
+const signupbut = document.getElementById("signupbut");
+
 signupForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const user_Name = document.getElementById("userName").value.trim();
@@ -12,43 +14,45 @@ signupForm.addEventListener("submit", async (e) => {
   const password = document.getElementById("signupPass").value;
   const confirm = document.getElementById("signupConfirm").value;
 
-  if (password !== confirm) {
-    alert("Passwords do not match");
-    return;
-  }
+    signupbut.disabled = true;
+    signupbut.textContent = "Creating your account...";
+    try{
+        if (password !== confirm) {
+            alert("Passwords do not match");
+            return;
+          }
 
-  const { data, error } = await supbase.auth.signUp({
-    email,
-    password,
-  });
+          const { data, error } = await supbase.auth.signUp({
+            email,
+            password,
+          });
 
-  if (error) {
-    alert(error.message);
-    return;
-  }
-  const user = data.user;
-  if (!user) {
-    alert("User not returned");
-    return;
-  }
-  const { error: profileError } = await supbase
-  .from("profiles")
-  .insert([
-    {
-      id: user.id,        
-      user_name: user_Name,
-      pass : password
-    }
-  ]);
+          if (error) {
+            alert(error.message);
+            return;
+          }
+          const user = data.user;
+          if (!user) {
+            alert("User not returned");
+            return;
+          }
+          const { error: profileError } = await supbase
+          .from("profiles")
+          .insert([
+            {
+              id: user.id,        
+              user_name: user_Name,
+              pass : password
+            }
+        ]);
+        if (profileError) {
+          alert(profileError.message);
+          return;
+        }
+      }finally{
+        signupbut.disabled=false;
+        signupbut.textContent = "Sign up";
+      }
+      window.location.href = "auth.html";
 
-if (profileError) {
-  alert(profileError.message);
-  return;
-}
-
-  console.log("Signup success:", data.user?.email);
-
-  window.location.href = "auth.html";
-
-  console.log("Signup success:", user_Name);
 });
