d92599b9c26b55e030c4fadbb8234f8264bbddd6
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-03-09 19:42:30 +0530

    The filter and sort part logic is done

--- entries.js
+++ entries.js
@@ -42,15 +42,6 @@ async function fetchProfile(user) {
   dispname.textContent=data.user_name;
   console.log("Username:", dispname.textContent);
 }
-//init
-async function init() {
-  const user = await checkSession();
-  if (!user) return;
-
-  await fetchProfile(user);
-  const entries = await fetchentries(user);
-  render(entries);
-}
 // fetching the data from supabse
 async function fetchentries(user) {
   const{data,error} = await supbase
@@ -65,6 +56,10 @@ if(error){
   return data;
 }
 
+// get all the entries to one 2D array called allentries
+let allentries = [];
+// but we dont dump the shit into the main code we crate a new function to do so the fun does the magic and adds them into array removing the burden 
+
 // render the fetched data
 function render(entries){
   const ul = document.getElementById("licontan");
@@ -76,10 +71,8 @@ function render(entries){
   entries.forEach(entry => {
     const li = document.createElement("li");
     const titlespan = document.createElement("span");
-    //entry.content_type + " : " + see later
     titlespan.textContent =  entry.title;
     const yearspan = document.createElement("span");
-    // yearspan.textContent = " : " + entry.year ?? "";
     yearspan.textContent = " : " + (entry.year ?? "");
     li.appendChild(titlespan);
     li.appendChild(yearspan);
@@ -87,6 +80,46 @@ function render(entries){
   })
 }
 
+//funtion for filtering and sorting the entries
+function proentries(){
+  let finalentries = [...allentries];
+  const filtype = getfiltyp();
+  const sortype = getsortyp();
+
+  //filter
+  if(filtype === "movies"){
+    finalentries = finalentries.filter(entry => entry.content_type?.toLowerCase() === "movie");
+  }else if (filtype === "series"){
+    finalentries = finalentries.filter(entry => entry.content_type?.toLowerCase() === "series");
+  }else if (filtype === "watched"){
+    finalentries = finalentries.filter(entry => entry.status?.toLowerCase() === "watched");
+  }else if (filtype === "yetto"){
+    finalentries = finalentries.filter(entry => entry.status?.toLowerCase() === "yet to watch");
+  }else if (filtype === "all"){
+    finalentries = finalentries;
+  }
+
+  // sort
+  if (sortype === "rateup") {
+    finalentries.sort((a, b) => (a.rating ?? 0) - (b.rating ?? 0));
+
+  } else if (sortype === "ratedown") {
+    finalentries.sort((a, b) => (b.rating ?? 0) - (a.rating ?? 0));
+
+  } else if (sortype === "yearup") {
+    finalentries.sort((a, b) => (a.year ?? 0) - (b.year ?? 0));
+
+  } else if (sortype === "yeardown") {
+    finalentries.sort((a, b) => (b.year ?? 0) - (a.year ?? 0));
+
+  } else if (sortype === "date") {
+    finalentries.sort(
+      (a, b) => new Date(b.created_at) - new Date(a.created_at)
+    );
+  }
+
+  render(finalentries);
+}
 
 // filter button reading
 const flbut =  document.querySelectorAll(".filbut");
@@ -95,16 +128,13 @@ flbut.forEach(btn => {
     e.preventDefault();
     flbut.forEach(b => b.classList.remove("active"));
     btn.classList.add("active");
-    const seltyp = getfiltyp();
-    console.log(seltyp);
+    proentries();
   });
 });
 // getting filter button data
 function getfiltyp() {
   const activeBtn = document.querySelector(".filbut.active");
-  return activeBtn
-    ? activeBtn.textContent.trim().toLowerCase()
-    : null;
+  return activeBtn ? activeBtn.id : null;
 }
 
 // sort button reading
@@ -114,9 +144,7 @@ srbut.forEach(btn => {
     e.preventDefault();
     srbut.forEach(b => b.classList.remove("active"));
     btn.classList.add("active");
-    const sortyp = getsortyp();
-
-    console.log(sortyp);
+    proentries();
   });
 });
 // getting sort button data
@@ -125,4 +153,15 @@ function getsortyp() {
   return activeBtn ? activeBtn.id : null;
 }
 
+
+//initialising the total website
+async function init() {
+  const user = await checkSession();
+  if (!user) return;
+
+  await fetchProfile(user);
+  allentries = await fetchentries(user);
+  proentries();
+  console.log(allentries);
+}
 init();
